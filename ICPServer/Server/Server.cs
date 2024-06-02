c28153fc6810052783d42d46a00155e35e43b2d8
Victor <victor.galves@live.com>
2024-06-02 20:30:09 -0300

    Structure rework

---
+++ ICPServer/Server/Server.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Net.Sockets;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ICPServer.Data
+{
+    public class Server
+    {
+        public static IHost HostBuilder(string Port)
+        {
+            return Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                webHostBuilder.UseUrls($"http://*:{Port}");
+                webHostBuilder.UseStartup<ServerConfig>();
+            }).Build();
+        }
+
+        private class ServerConfig
+        {
+            public static void ConfigureServices(IServiceCollection services)
+            {
+                services.AddControllers();
+            }
+
+            public static void Configure(IApplicationBuilder app)
+            {
+                app.UseRouting();
+
+                app.UseEndpoints(routes =>
+                {
+                    routes.MapControllers();
+                });
+            }
+        }
+    }
+
+}
